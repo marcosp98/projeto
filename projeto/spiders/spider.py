c1d16b2405d9ee3f75e3c0c5608d13de76c63475
marcosppaulo98 <161390830+marcosppaulo98@users.noreply.github.com>
2024-09-18 17:01:20 -0300

    Adicionar arquivos do projeto Scrapy

---
+++ projeto/spiders/spider.py
@@ -0,0 +1,28 @@
+import scrapy
+
+class KabumSpider(scrapy.Spider):
+    name = 'kabum'
+    allowed_domains = ['mercadolivre.com.br'] #['kabum.com.br']
+    start_urls = ['https://lista.mercadolivre.com.br/memoria-ram#D[A:memoria%20ram]']#['https://www.kabum.com.br/hardware/memoria-ram']
+
+    def parse(self, response):
+        print("[ parse ]")
+        produtos = response.css('li.ui-search-layout__item')
+        
+        for produto in produtos:
+            titulo = produto.css('h2.ui-search-item__title a::text').get()
+            link = produto.css('a.ui-search-link::attr(href)').get()
+
+            reais = produto.css('.andes-money-amount__fraction::text').get()
+            centavos = produto.css('.andes-money-amount__cents::text').get()
+
+            preco = f"{reais},{centavos}" if centavos else f"{reais}"
+            
+            imagem_url = produto.css('img.ui-search-result-image__element::attr(src)').get()
+
+
+            print(f"Título: {titulo}")
+            print(f"Link: {link}")
+            print(f"Preço: R$ {preco}")
+            print(f"Imagem URL: {imagem_url}")
+
