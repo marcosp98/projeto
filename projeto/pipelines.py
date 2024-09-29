c0dd58663e22572aff5ea638badcd415bea37af4
marcosppaulo98 <161390830+marcosppaulo98@users.noreply.github.com>
2024-09-29 17:45:42 -0300

    Sua mensagem de commit

--- projeto/pipelines.py
+++ projeto/pipelines.py
@@ -5,31 +5,33 @@ class CsvPipeline:
     def __init__(self):
         self.file_path = 'output/produtos.csv'
         
-        # Verificar se o arquivo existe e tem conteúdo
+        # Verificar se o arquivo já existe e tem conteúdo
         if os.path.exists(self.file_path):
             with open(self.file_path, 'r', encoding='utf-8') as file:
                 reader = csv.reader(file)
-                # Conta o número de linhas no CSV, ignorando o cabeçalho
                 rows = list(reader)
-                if len(rows) > 1:  # Se tiver produtos no CSV
-                    self.id_counter = len(rows)  # Continua a contagem
+                # Continua a contagem de IDs a partir da última linha do CSV (se houver)
+                if len(rows) > 1:
+                    self.id_counter = len(rows)  # Inicia o ID a partir do último valor
                 else:
-                    self.id_counter = 1  # Reinicia do 1
+                    self.id_counter = 1  # Começa do 1 se o CSV estava vazio
         else:
-            self.id_counter = 1  # Reinicia do 1 se não houver arquivo
-
-        # Abrir o arquivo para adicionar novos dados
+            self.id_counter = 1  # Começa do 1 se o CSV não existe
+        
+        # Abre o arquivo para adicionar novos dados
         self.file = open(self.file_path, 'a', newline='', encoding='utf-8')
         self.writer = csv.writer(self.file)
 
-        # Se o arquivo foi apagado, escrever o cabeçalho novamente
+        # Se o arquivo estava vazio, escreve o cabeçalho
         if os.stat(self.file_path).st_size == 0:
-            self.writer.writerow(['id', 'titulo', 'link', 'preco', 'img'])  # Cabeçalho
+            self.writer.writerow(['id', 'titulo', 'link', 'preco', 'img', 'nota', 'qtd_aval'])  # Cabeçalho
 
     def process_item(self, item, spider):
-        item['id'] = self.id_counter  # Atribuir ID único
-        self.writer.writerow([item['id'], item['titulo'], item['link'], item['preco'], item['img']])
-        self.id_counter += 1
+        # Atribuir o ID ao item e escrever no CSV
+        item['id'] = self.id_counter
+        item['site'] = spider.allowed_domains[0]  # Capturar o site (domínio) da spider
+        self.writer.writerow([item['id'], item['titulo'], item['link'], item['preco'], item['img'], item['nota'], item['qtd_aval'], item['site']])
+        self.id_counter += 1  # Incrementar o ID para o próximo item
         return item
 
     def close_spider(self, spider):
--- projeto/spiders/spider.py
+++ projeto/spiders/spider.py
@@ -1,42 +1,85 @@
+# import scrapy
+
+# desejo = input("O que você deseja?")
+
+# class MLSpider(scrapy.Spider):
+#     name = 'mercadolivre'
+#     allowed_domains = ['mercadolivre.com.br'] #['kabum.com.br']
+#     start_urls = ['https://lista.mercadolivre.com.br/'+ desejo]#['https://www.kabum.com.br/hardware/memoria-ram']
+    
+#     def parse(self, response):
+#         print("[ parse ]")
+#         produtos = response.css('li.ui-search-layout__item')
+        
+#         for produto in produtos:
+#             titulo = produto.css('h2.ui-search-item__title a::text').get()
+#             link = produto.css('a.ui-search-link::attr(href)').get()
+#             reais = produto.css('.andes-money-amount__fraction::text').get()
+#             centavos = produto.css('.andes-money-amount__cents::text').get()
+#             preco = f"{reais},{centavos}" if centavos else f"{reais}"
+#             imagem_url = produto.css('img.ui-search-result-image__element::attr(src)').get()
+#             nota_avaliacao = produto.css('span.ui-search-reviews__rating-number::text').get()
+#             quantidade_opinioes = produto.css('span.ui-search-reviews__amount::text').get()
+            
+#             yield {
+#                 'titulo': titulo,
+#                 'link': link,
+#                 'preco': preco,
+#                 'img': imagem_url,
+#                 'nota': nota_avaliacao,
+#                 'qtd_aval': quantidade_opinioes
+#             }
+            
+            
+#         next_page = response.css('li.andes-pagination__button--next a::attr(href)').get()
+
+#         if next_page:
+#             next_page_url = response.urljoin(next_page)  # Garante que a URL seja absoluta
+#             yield scrapy.Request(url=next_page_url, callback=self.parse, dont_filter=True)
+#             # print(f"Link: {link}")
+#             # print(f"Preço: R$ {preco}")
+#             # print(f"Imagem URL: {imagem_url}")
+
 import scrapy
 
 
-class KabumSpider(scrapy.Spider):
-    desejo = input("O que você deseja?")
-    name = 'kabum'
-    allowed_domains = ['mercadolivre.com.br'] #['kabum.com.br']
-    start_urls = ['https://lista.mercadolivre.com.br/'+ desejo]#['https://www.kabum.com.br/hardware/memoria-ram']
-    
+class VerificaRestricoesSpider(scrapy.Spider):
+    name = "test"
+    allowed_domains = [
+        "amazon.com.br",
+        "kabum.com.br",
+        "pichau.com.br",
+        "magazineluiza.com.br",
+        "terabyte.com.br"
+    ]
+    start_urls = [
+        "https://www.amazon.com.br/robots.txt",
+        "https://www.kabum.com.br/robots.txt",
+        "https://www.pichau.com.br/robots.txt",
+        "https://www.magazineluiza.com.br/robots.txt",
+        "https://terabyte.com.br/robots.txt"
+    ]
+
     def parse(self, response):
-        print("[ parse ]")
-        produtos = response.css('li.ui-search-layout__item')
+        site = response.url
+        self.logger.info(f"Verificando {site}")
         
-        id_counter = 1
+        # Extrai o conteúdo do robots.txt
+        conteudo = response.text.strip().splitlines()
+        
+        # Inicializa as listas de restrições
+        allow = []
+        disallow = []
 
-        for produto in produtos:
-            titulo = produto.css('h2.ui-search-item__title a::text').get()
-            link = produto.css('a.ui-search-link::attr(href)').get()
-            reais = produto.css('.andes-money-amount__fraction::text').get()
-            centavos = produto.css('.andes-money-amount__cents::text').get()
-            preco = f"{reais},{centavos}" if centavos else f"{reais}"
-            imagem_url = produto.css('img.ui-search-result-image__element::attr(src)').get()
+        # Processa as linhas do robots.txt
+        for linha in conteudo:
+            linha = linha.strip()
+            if linha.startswith("Allow:"):
+                allow.append(linha[6:].strip())
+            elif linha.startswith("Disallow:"):
+                disallow.append(linha[9:].strip())
 
-            yield {
-                'id': id_counter,
-                'titulo': titulo,
-                'link': link,
-                'preco': preco,
-                'img': imagem_url
-            }
-            
-            id_counter += 1
-            
-        next_page = response.css('li.andes-pagination__button--next a::attr(href)').get()
-
-        if next_page:
-            next_page_url = response.urljoin(next_page)  # Garante que a URL seja absoluta
-            yield scrapy.Request(url=next_page_url, callback=self.parse, dont_filter=True)
-            # print(f"Link: {link}")
-            # print(f"Preço: R$ {preco}")
-            # print(f"Imagem URL: {imagem_url}")
+        # Exibe as restrições
+        self.logger.info(f"Permissões em {site}: {allow}")
+        self.logger.info(f"Restrições em {site}: {disallow}")
 
